4f4379405e828b9134303268348c4a1abd34d451
Albatross3 <superstring77@gmail.com>
2023-02-09 17:46:08 +0900

    feat : WebSocket 설정 파일 구현

--- src/main/java/com/random/ChatRoom/config/WebSocketConfig.java
+++ src/main/java/com/random/ChatRoom/config/WebSocketConfig.java
@@ -1,25 +1,25 @@
 package com.random.ChatRoom.config;
 
-import com.random.ChatRoom.handler.ChatHandler;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+
+@EnableWebSocketMessageBroker
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
-    // URL 과 handler 를 연결해준다 딱 한번
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatHandler(), "/chat").setAllowedOrigins("*");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/anonymous-chat").setAllowedOrigins("*");
     }
 
-    @Bean
-    public WebSocketHandler chatHandler() {
-        return new ChatHandler();
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/subscribe");
+        registry.setApplicationDestinationPrefixes("/publish");
     }
-
 }
