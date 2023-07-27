354b64a4406e3128c6c3c0cedde1d6ba9e9cf581
Albatross3 <superstring77@gmail.com>
2023-07-27 22:32:43 +0900

    refactor : 생성자 주입 lombok 어노테이션으로 대체
    - 의존관계는 필드 값으로 파악할 수 있다고 보기에 생성자를 lombok 으로 대체하여 가독성을 높였습니다

--- src/main/java/com/random/ChatRoom/core/member/service/MemberService.java
+++ src/main/java/com/random/ChatRoom/core/member/service/MemberService.java
@@ -3,21 +3,21 @@ package com.random.ChatRoom.core.member.service;
 import com.random.ChatRoom.core.member.domain.Member;
 import com.random.ChatRoom.core.member.domain.MemberRepository;
 import com.random.ChatRoom.core.member.dto.MemberRequest;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
 @Service
 public class MemberService {
 
   private final MemberRepository memberRepository;
-
-  public MemberService(MemberRepository memberRepository) {
-    this.memberRepository = memberRepository;
-  }
 
   @Transactional
   public void saveMember(MemberRequest memberRequest) {
     Member member = memberRequest.toMember();
     memberRepository.save(member);
   }
+
 }
--- src/main/java/com/random/ChatRoom/core/question/service/QuestionService.java
+++ src/main/java/com/random/ChatRoom/core/question/service/QuestionService.java
@@ -3,17 +3,16 @@ package com.random.ChatRoom.core.question.service;
 import com.random.ChatRoom.core.question.domain.Question;
 import com.random.ChatRoom.core.question.domain.QuestionRepository;
 import java.util.Map;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
 @Service
 public class QuestionService {
 
   private final QuestionRepository questionRepository;
-
-  public QuestionService(QuestionRepository questionRepository) {
-    this.questionRepository = questionRepository;
-  }
 
   @Transactional
   public void createQuestionList(Map<String, String> paramMap) {
