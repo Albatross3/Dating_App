637628ee5a6e05acffbd062fc292de1b7d5de634
Albatross3 <superstring77@gmail.com>
2023-07-31 22:27:13 +0900

    feat : member, question 양방향 연관관계 추가

--- src/main/java/com/random/ChatRoom/core/member/domain/Member.java
+++ src/main/java/com/random/ChatRoom/core/member/domain/Member.java
@@ -1,6 +1,7 @@
 package com.random.ChatRoom.core.member.domain;
 
 
+import com.random.ChatRoom.core.question.domain.Question;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -8,6 +9,9 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -35,6 +39,9 @@ public class Member {
   @Column(nullable = false)
   @Enumerated(EnumType.STRING)
   private Major major;
+
+  @OneToMany(mappedBy = "member")
+  private List<Question> questions = new ArrayList<>();
 
   @Builder
   public Member(Long memberId, Sex sex, String nickname, int age, Major major) {
--- src/main/java/com/random/ChatRoom/core/question/domain/Question.java
+++ src/main/java/com/random/ChatRoom/core/question/domain/Question.java
@@ -1,13 +1,19 @@
 package com.random.ChatRoom.core.question.domain;
 
+import com.random.ChatRoom.core.member.domain.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Question {
@@ -19,7 +25,13 @@ public class Question {
   @Column(nullable = false)
   private String question;
 
-  public Question(String question) {
+  @ManyToOne
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @Builder
+  public Question(String question, Member member) {
     this.question = question;
+    this.member = member;
   }
 }
