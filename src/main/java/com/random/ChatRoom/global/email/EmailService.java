ca4a6214ce9fffd069db8aff9d1d77e4aecdd7b4
Albatross3 <superstring77@gmail.com>
2023-09-07 13:34:06 +0900

    refactor: 이메일 발송 기능 리팩터링
    - HTML 문서와 데이터의 분리
    - 이메일 만들기와 전송에 대한 예외처리

--- src/main/java/com/random/ChatRoom/global/email/EmailService.java
+++ src/main/java/com/random/ChatRoom/global/email/EmailService.java
@@ -1,50 +1,56 @@
 package com.random.ChatRoom.global.email;
 
+import com.random.ChatRoom.core.common.exceptions.ErrorCode;
+import com.random.ChatRoom.global.email.exceptions.EmailCreationFailedException;
+import com.random.ChatRoom.global.email.exceptions.EmailSendFailedException;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
   private final JavaMailSender javaMailSender;
-  private final Random random = new Random();
-  private final String subject = "MakeLover 인증코드 메일 💌";
-  private final String from = "makeloverwithus@naver.com";
-  private final String content =
-      "<h2>MakeLover 인증코드입니다 <br> 서비스에 돌아가서 입력해 주세요</h2>\n"
-      + "<h3>재현 안뇽~!  </h3>"
-      + "<img src=\"cid:love-image\" alt=\"대체 텍스트\">";
+  private final SpringTemplateEngine springTemplateEngine;
 
-  public String sendEmail(String toEmail) {
+  public void sendEmailForRegister(String toEmail) {
     MimeMessage mimeMessage = javaMailSender.createMimeMessage();
     String verificationCode = createVerificationCode();
 
     try {
       MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
 
-      mimeMessageHelper.setSubject(subject);
-      mimeMessageHelper.setFrom(from);
+      mimeMessageHelper.setSubject("MakeLover 인증코드 메일 💌");
+      mimeMessageHelper.setFrom("makeloverwithus@naver.com");
       mimeMessageHelper.setTo(toEmail);
 
-      mimeMessageHelper.setText(content, true);
+      Context context = new Context();
+      context.setVariable("code", verificationCode);
+
+      String emailContent = springTemplateEngine.process("mail.html", context);
+      mimeMessageHelper.setText(emailContent, true);
       mimeMessageHelper.addInline("love-image", new ClassPathResource("static/love.png"));
 
       javaMailSender.send(mimeMessage);
 
     } catch (MessagingException e) {
-      throw new RuntimeException(e);
+      throw new EmailCreationFailedException(ErrorCode.EMAIL_CREATION_FAILED);
+    } catch (MailException e) {
+      throw new EmailSendFailedException(ErrorCode.EMAIL_SEND_FAILED);
     }
-    return "mail";
   }
 
   private String createVerificationCode() {
+    Random random = new Random();
     StringBuilder sb = new StringBuilder();
     for (int i = 0; i < 4; i++) {
       sb.append(random.nextInt(10));
